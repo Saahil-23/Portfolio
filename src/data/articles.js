a155b9191dddc322af1da2037ed733a5e58053b5
Saahil-23 <saahil.sawant24@gmail.com>
2025-10-16 17:44:24 +0530

    CCIDF article linked

--- src/data/articles.js
+++ src/data/articles.js
@@ -5,7 +5,7 @@ export const articlesData = [
   {
     title: 'A Hands-On Introduction to Cybersecurity with TryHackMe',
     description: 'This article walks you through the fundamentals of cybersecurity, providing a practical, hands-on experience by following the popular Cybersecurity 101 path on TryHackMe.',
-    link: 'https://your-blog-platform.com/article-1',
+    link: 'https://github.com/Saahil-23/Portfolio/blob/main/public/CCIDF.md',
   },
   {
     title: 'Applications of Finite Automata in Text Search and Pattern Matching',
