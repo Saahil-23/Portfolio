6d58ce2c49c00c9482f5ea2e0272d19f052e75d2
Saahil-23 <saahil.sawant24@gmail.com>
2025-10-21 14:17:40 +0530

    Videos part 2

--- src/data/videos.js
+++ src/data/videos.js
@@ -20,5 +20,23 @@ export const videosData = [
     title: 'How Emotional Intelligence Improves Team Collaboration',
     description: 'This video explores how emotional intelligence (EI) training enhances team collaboration in corporate environments by improving communication and conflict resolution.'
   },
+  {
+    id: 4,
+    embedUrl: 'https://www.youtube.com/embed/RDKvrgJbN4E',  
+    title: 'Regular Expressions in Modern Compilers and Programming Languages',
+    description: 'This video explores the role of regular expressions in modern compilers and programming languages. '
+  },
+  {
+    id: 5,
+    embedUrl: 'https://www.youtube.com/embed/qD7cjpnyG9E',
+    title: 'Unveiling Insights: A Deep Dive into Data Warehousing and Data Mining',
+    description: 'This video explores the fundamentals of Data Warehousing and Data Mining, covering their architecture, key concepts, techniques, tools, and real-world applications.'
+  },
+  {
+    id: 6,
+    embedUrl: 'https://www.youtube.com/embed/9nWX6u60v8I',
+    title: 'A Hands-On Introduction to Cybersecurity with TryHackMe',
+    description: 'This video walks you through the fundamentals of cybersecurity, providing a practical, hands-on experience by following the popular Cybersecurity 101 path on TryHackMe.'
+  },
   
 ];
